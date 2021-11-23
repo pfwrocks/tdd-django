93c88408f955bdab0beff898f3965ca89f6492e3
pfwrocks <pfwrocks@gmail.com>
2021-11-23 11:19:05 -0500

    first FT commented out, using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http:localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_build_list_and_retrieve_later(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('To-Do', self.browser.title)  
+		self.fail('Finish the test!')
+
+		# able to enter to-do
+
+		# enter "Buy peacock feathers" into textbox
+
+		# can view updated item
+
+		# enter "Make fly" into textbox from same page
+
+		# can see both items
+
+		# closes & revists url to confirm to-do list exists
+
+if __name__ == '__main__':
+	unittest.main()
