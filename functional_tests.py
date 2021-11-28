f0ea3e3a4289c5d348d69acbd844c025e118baee
pfwrocks <pfwrocks@gmail.com>
2021-11-27 20:40:04 -0500

    added input to-do item checks for functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+import unittest, test
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -12,16 +13,32 @@ class NewVisitorTest(unittest.TestCase):
 	def test_build_list_and_retrieve_later(self):
 		self.browser.get('http://localhost:8000')
 
-		self.assertIn('To-Do', self.browser.title)  
-		self.fail('Finish the test!')
+		# header and title mention To-Do
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		# able to enter to-do
+		input_box = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			input_box.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
 		# enter "Buy peacock feathers" into textbox
+		input_box.send_keys('Buy peacock feathers')
+		input_box.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		# can view updated item
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any( row.text == '1: Buy peacock feathers' for row in rows )
+		)
 
 		# enter "Make fly" into textbox from same page
+		self.fail('Finish test!')
 
 		# can see both items
 
