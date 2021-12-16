44dcc081f5166854a5975ebf232966be0f8714dd
pfwrocks <pfwrocks@gmail.com>
2021-12-16 00:28:27 -0500

    moved lists UTs into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -10,34 +10,6 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
-class ListAndItemModelsTest(TestCase):
-	def test_saving_and_retreiving_items(self):
-		lst = List()
-		lst.save()
-
-		first_item = Item()
-		first_item.text = 'first test item'
-		first_item.list = lst
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = 'second test item'
-		second_item.list = lst
-		second_item.save()
-
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, lst)
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual('first test item', first_saved_item.text)
-		self.assertEqual(first_saved_item.list, lst)
-		self.assertEqual('second test item', second_saved_item.text)
-		self.assertEqual(second_saved_item.list, lst)
-
 class ListViewTest(TestCase):
 	def test_uses_list_template(self):
 		lst = List.objects.create()
